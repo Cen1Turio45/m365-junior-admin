68fdd2b70fa6f08188083b4428baefc0c5ccf132
Cen1Turio45 <lucaswessner12@gmail.com>
2025-04-24 20:36:52 +0200

    Update and rename install-m365-modules.ps1 to ErstinstallationPowershell+M365.ps1

---
+++ M365_Powershell_Skripte/ErstinstallationPowershell+M365.ps1
@@ -0,0 +1,13 @@
+Bei Erstinstallation diese Module installieren:
+
+Install-Module Microsoft.Graph -Scope CurrentUser -Force
+Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force
+Install-Module MicrosoftTeams -Scope CurrentUser -Force
+Install-Module SharePointPnPPowerShellOnline -Scope CurrentUser -Force
+
+Bei Erstinstalltion auch mit allem einmal verbinden:
+
+Connect-MgGraph -Scopes "User.ReadWrite.All", "Group.ReadWrite.All", "Directory.ReadWrite.All"
+Connect-ExchangeOnline
+Connect-MicrosoftTeams
+
