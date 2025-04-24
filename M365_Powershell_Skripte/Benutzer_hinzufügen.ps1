00c3943469cd703324d84e7c3a16dd79e22cebbf
Cen1Turio45 <lucaswessner12@gmail.com>
2025-04-24 20:57:38 +0200

    Update Benutzer_hinzufügen.ps1

--- M365_Powershell_Skripte/Benutzer_hinzufügen.ps1
+++ M365_Powershell_Skripte/Benutzer_hinzufügen.ps1
@@ -1,5 +1,11 @@
 # Hinweis: Um Module zu laden + anmelden schau bei Erstinstallation M365
 
+#CSV Sheet Beispiel:
+DisplayName,GivenName,Surname,UserPrincipalName,MailNickname,Password,UsageLocation
+Max Mustermann,Max,Mustermann,max.mustermann@tenant.onmicrosoft.com,max.mustermann,Start123!,DE
+Julia Beispiel,Julia,Beispiel,julia.beispiel@tenant.onmicrosoft.com,julia.beispiel,Passwort456!,DE
+
+
 # Mehrere Benutzer über CSV hinzufügen:
 $csvPath = Read-Host "Pfad zur CSV-Datei eingeben (z. B. C:\Benutzer\users.csv)"
 $users = Import-Csv -Path $csvPath
