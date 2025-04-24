7961e7b8be6c7034c84191ea0015cd7e09dec59a
Cen1Turio45 <lucaswessner12@gmail.com>
2025-04-24 20:39:00 +0200

    Update ErstinstallationPowershell+M365.ps1

--- M365_Powershell_Skripte/ErstinstallationPowershell+M365.ps1
+++ M365_Powershell_Skripte/ErstinstallationPowershell+M365.ps1
@@ -5,6 +5,7 @@ Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force
 Install-Module MicrosoftTeams -Scope CurrentUser -Force
 Install-Module SharePointPnPPowerShellOnline -Scope CurrentUser -Force
 
+
 # Bei Erstinstalltion auch mit allem einmal verbinden:
 
 Connect-MgGraph -Scopes "User.ReadWrite.All", "Group.ReadWrite.All", "Directory.ReadWrite.All"
